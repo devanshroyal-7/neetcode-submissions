52d8be61f30fe3cd887a1dc116ecdae924ff3966
Devansh Royal J <jdevansh2000@gmail.com>
2026-05-15 12:17:24 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(t) < len(s):
+            return False
+        
+        if sorted(s) == sorted(t):
+            return True
+        return False
